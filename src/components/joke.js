18069e9287916b4dd4748ce661c0a6841664e2a7
drewatienza <atienza.drew@gmail.com>
2020-10-09 10:19:00 -0700

    added styling to joke component

--- src/components/joke.js
+++ src/components/joke.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
+import './joke.css'
+
 class Joke extends Component {
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote} />
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes">{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote} />
         </div>
         <div className="Joke-text">{this.props.text}</div>
+        <div className='Joke-smiley'>
+          <i class="em em-rolling_on_the_floor_laughing" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+        </div>
       </div>
     );
   }
