8657383aafc97622360c53df8a6cd726b6173173
drewatienza <atienza.drew@gmail.com>
2020-10-09 13:51:57 -0700

    added dynamic colors and emojis

--- src/components/joke-list.js
+++ src/components/joke-list.js
@@ -28,11 +28,9 @@ class JokeList extends Component {
   }
 
   handleVote(id, delta) {
-    this.setState((st) => ({
-      jokes: st.jokes.map((j) =>
-        j.id === id ? { ...j, votes: j.votes + delta } : j
-      ),
-    }));
+    this.setState(st => ({
+      jokes: st.jokes.map(j => j.id === id ? {...j, votes: j.votes + delta} : j)
+    }))
   }
 
   render() {
